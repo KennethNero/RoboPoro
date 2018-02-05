16cdaa98b202a1dc5e7180d3eab81018513744a2
Kenenth Nero <kbn1798@rit.edu>
2018-02-05 13:18:57 -0500

    (slash) poro command now functional with 5 whole poros. Basic functionality acheived. We can go bigger.

--- src/main/java/com/kbn1798/BotCmdTest.java
+++ src/main/java/com/kbn1798/BotCmdTest.java
@@ -1,9 +1,16 @@
 package com.kbn1798;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.EmbedBuilder;
 import sx.blah.discord.util.RequestBuffer;
 
 class BotCmdTest {
@@ -21,6 +28,42 @@ class BotCmdTest {
                 .build();
 
     }
+    
+    /**
+     * Sends a file from URL with a given message.
+     * 
+     * @param channel Channel from which the event is being handled
+     * @throws MalformedURLException Because people suck with files
+     * @throws IOException And occasionally people suck with files
+     */
+    static void sendFileFromURL(IChannel channel, String url, String msgText, String fileName) throws MalformedURLException, IOException {
+    	InputStream input = new URL(url).openStream();
+    	RequestBuffer.request(() ->{
+    		try {
+    			channel.sendFile(msgText, false, input, fileName);
+    		}catch (DiscordException e){
+    			System.err.println("File could not be sent with error: ");
+    			e.printStackTrace();
+    			
+    		}
+    	});
+    }
+    
+    static void embedFileFromURL(IChannel channel, String url) throws MalformedURLException, IOException{
+    	EmbedBuilder i = new EmbedBuilder();
+    	i.withImage(url);
+    	EmbedObject e = i.build();
+    	RequestBuffer.request(() ->{
+    		try {
+    			channel.sendMessage(e);
+    		}catch (DiscordException ex){
+    			System.err.println("File could not be sent with error: ");
+    			ex.printStackTrace();
+    			
+    		}
+    	});
+    }
+    
 
     // Helper functions to make certain aspects of the bot easier to use.
     static void sendMessage(IChannel channel, String message){
@@ -34,6 +77,7 @@ class BotCmdTest {
                 e.printStackTrace();
             }
         });
+        
 
 		/*
 		// The below example is written to demonstrate sending a message if you want to catch the RLE for logging purposes
--- src/main/java/com/kbn1798/MainRunner.java
+++ src/main/java/com/kbn1798/MainRunner.java
@@ -1,11 +1,17 @@
 package com.kbn1798;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import sx.blah.discord.api.IDiscordClient;
 
 public class MainRunner {
+	
+	public static HashMap<String, ArrayList<String>> listOLists = new HashMap<>();
 
     public static void main(String[] args){
-
+    	populateHashmap();
+    	
         if(args.length != 1){
             System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
             return;
@@ -31,6 +37,16 @@ public class MainRunner {
         // Only login after all events are registered otherwise some may be missed.
         cli.login();
 
+    }
+    
+    private static void populateHashmap() {
+    	ArrayList<String> l = new ArrayList<String>();
+    	l.add("https://i.imgur.com/qGWNJI6.jpg");
+    	l.add("https://i.imgur.com/eY7722M.jpg");
+    	l.add("https://i.imgur.com/YHEneSA.jpg");
+    	l.add("https://i.imgur.com/IRz71Sq.png");
+    	l.add("https://i.imgur.com/Ir8OP15.png");
+    	listOLists.put("Poro", l);
     }
 
 }
