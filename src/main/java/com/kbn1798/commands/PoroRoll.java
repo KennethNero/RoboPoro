195fbed2c7f693f20e9b6d4080f2952f41bfb9d0
Kenenth Nero <kbn1798@rit.edu>
2018-02-11 18:37:35 -0500

    Fixed log error & added roll base.
    Added logback-classic to remove an error upon startup of the bot. Need
    to work on fixing a reflection error now, and then startup should be
    totally clean. Also added the base class for a roll command.

---
+++ src/main/java/com/kbn1798/commands/PoroRoll.java
@@ -0,0 +1,12 @@
+package com.kbn1798.commands;
+
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+public class PoroRoll {
+
+	public static void processRoll(MessageReceivedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/com/kbn1798/core/MyEvents.java
+++ src/main/java/com/kbn1798/core/MyEvents.java
@@ -3,6 +3,7 @@ package com.kbn1798.core;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import com.kbn1798.commands.PoroRoll;
 import com.kbn1798.commands.PoroShow;
 import com.kbn1798.utils.BotUtils;
 
@@ -15,11 +16,10 @@ public class MyEvents {
     public void onMessageReceived(MessageReceivedEvent event) throws MalformedURLException, IOException{
         if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX + "test"))
             BotUtils.sendMessage(event.getChannel(), "I am sending a message from an EventSubscriber listener");
-        else if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX + "poro")) {
+        else if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX + "poro"))
         	PoroShow.providePoro(event);
-        	
-        }
-        	
+        else if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX + "roll"));
+        	PoroRoll.processRoll(event);
     }
 
 }
